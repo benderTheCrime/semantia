0393844cbae559acb8c01d5c978a9bdabd039e16
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-11-04 00:13:33 -0500

    Added fixes for runner

--- examples/number.js
+++ examples/number.js
@@ -8,6 +8,9 @@ class Number {
     static double(n) {
         return 2 * n;
     }
+    static triple(n) {
+        return 3 * n;
+    }
 }
 
 export default Number;
---
+++ examples/test/number2.spec.js
@@ -0,0 +1,19 @@
+// Semantia Modules
+import { default as semantia } from     '../../lib/index';
+import { default as number } from       '../number';
+
+semantia.testCase(number.triple, {
+    arguments: [
+        {
+            name: 'number',
+            type: 'number',
+            order: 10,
+            signed: true
+        }
+    ],
+    callCount: 100,
+    expected: {
+        type: 'number',
+        value: '3 * number'
+    }
+});
