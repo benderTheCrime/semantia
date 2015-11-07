e983db9fe31d2b334a8ad95cbf5d3610cc9f022e
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-11-07 18:21:33 -0500

    Added string passing

--- examples/test/number2.spec.js
+++ examples/test/number2.spec.js
@@ -2,7 +2,21 @@
 import { default as semantia } from     '../../lib/index';
 import { default as number } from       '../number';
 
-semantia.testCase(number.triple, {
+semantia.testCases(number.triple, {
+    arguments: [
+        {
+            name: 'number',
+            type: 'number',
+            order: 10,
+            signed: true
+        }
+    ],
+    callCount: 100,
+    expected: {
+        type: 'number',
+        value: '3 * number'
+    }
+}, {
     arguments: [
         {
             name: 'number',
