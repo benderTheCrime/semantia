5779382ebb5ae66fb41dfeea46ab316da68461a3
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-11-07 21:05:14 -0500

    Added fixes for tests

---
+++ examples/string.js
@@ -0,0 +1,16 @@
+/**
+ * @module string.js
+ * @author Joe Groseclose <@benderTheCrime>
+ * @date 11/7/2015
+ */
+
+class String {
+    static upper(s) {
+        return s.toUpperCase();
+    }
+    static lower(s) {
+        return s.toLowerCase();
+    }
+}
+
+export default String;
---
+++ examples/test/string.spec.js
@@ -0,0 +1,31 @@
+// Semantia Modules
+import { default as semantia } from     '../../lib/index';
+import { default as string } from       '../string';
+
+semantia.testCase(string.upper, {
+    arguments: [
+        {
+            name: 'string',
+            type: 'string',
+            mustHave: ':',
+            includes: 'abcde',
+            length: 20
+        }
+    ],
+    callCount: 100,
+    expected: {
+        type: 'string',
+        value: 'string.toUpperCase()'
+    }
+}).testCase(string.lower, {
+    arguments: [
+        {
+            name: 'string',
+            value: 'test'
+        }
+    ],
+    callCount: 3000,
+    expected: {
+        value: 'test'
+    }
+});
